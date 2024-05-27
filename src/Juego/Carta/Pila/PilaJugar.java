5deab7eb2f3248ab23768f4f45f8e466c7e85735
David <165419903+DavidY3@users.noreply.github.com>
2024-05-26 22:13:37 -0400

    Update PilaJugar.java
    Método validarCarta y jugarCarta en PilaJugar
    (Aún falta validar la primera carta que se coloca en la pila de Jugar y que si al jugador solo le queda una carta y es comodín debe agarrar una, que debe ser en otro sitio)

--- src/Juego/Carta/Pila/PilaJugar.java
+++ src/Juego/Carta/Pila/PilaJugar.java
@@ -8,11 +8,15 @@ import java.util.Stack;
 public class PilaJugar {
     private Stack<Carta> listaCartas = new Stack<Carta>();
 
-    public boolean validarCarta(Carta carta) {
-        return true;
+    public boolean validarCarta(Carta carta) {  //True=SI se puede jugar - False=NO se puede juegar
+        if(carta.getTipo() == "T4" || carta.getTipo() == "C")
+            return true;
+        if(listaCartas.peek().getColor() == carta.getColor() || listaCartas.peek().getTipo() == carta.getTipo())
+            return true;
+        return false;
     }
 
     public void jugarCarta(Carta carta) {
-
+        listaCartas.push(carta);
     }
 }
