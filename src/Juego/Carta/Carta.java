143ec99cf319a9b5cb45619b8f4a1b831cf55cfc
HumbertoAleman <haleman.23@est.ucab.edu.ve>
2024-06-02 10:38:16 -0400

    Quitado comentario de ejemplo de codigo

--- src/Juego/Carta/Carta.java
+++ src/Juego/Carta/Carta.java
@@ -6,11 +6,6 @@ public interface Carta {
      */
     public void ejecutarAccion();
 
-    // Al combinar la funcion getColor y getTipo, resulta la etiqueta que joselito especifico
-    // EJEMPLO:
-    // En una carta roja de skip, getColor retorna R, getTipo retorna S, se combinan y forma RS
-    // EJEMPLO 2:
-    // En una carta de comodin, cuyo color fue puesto en azul, getColor retorna B, getTipo retorna C
     /**
      * Obtiene el color de la carta
      * @return color de la carta
@@ -21,6 +16,7 @@ public interface Carta {
      * @return tipo de la carta
      */
     public String getTipo();
+
     /**
      * Muestra la carta
      */
