45e50dfd18e75909e4fbcff4f777a3e06cff21cf
HumbertoAleman <haleman.23@est.ucab.edu.ve>
2024-06-02 11:20:51 -0400

    Formateada la interfaz Carta.java

--- src/Juego/Carta/Carta.java
+++ src/Juego/Carta/Carta.java
@@ -4,21 +4,24 @@ public interface Carta {
     /**
      * Ejecuta la accion de la carta
      */
-    public void ejecutarAccion();
+    void ejecutarAccion();
 
     /**
      * Obtiene el color de la carta
+     *
      * @return color de la carta
      */
-    public char getColor();
+    char getColor();
+
     /**
      * Obtiene el tipo de la carta
+     *
      * @return tipo de la carta
      */
-    public String getTipo();
+    String getTipo();
 
     /**
      * Muestra la carta
      */
-    public void mostrarCarta();
+    void mostrarCarta();
 }
