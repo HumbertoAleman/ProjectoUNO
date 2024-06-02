0ef64b8b1305d068b9fe06c1e2f755566c8e5984
HumbertoAleman <haleman.23@est.ucab.edu.ve>
2024-06-02 10:39:31 -0400

    Refactorizada la funcion mostrarMazo en ImpresoraCarta.java

--- src/Juego/Jugador/ImpresoraCarta.java
+++ src/Juego/Jugador/ImpresoraCarta.java
@@ -2,8 +2,7 @@ package Juego.Jugador;
 
 import Juego.Carta.Carta;
 
-import java.util.ArrayList;
-import java.util.LinkedList;
+import java.util.*;
 
 public class ImpresoraCarta {
     public static final String ANSI_RESET = "\u001B[0m";
@@ -12,68 +11,32 @@ public class ImpresoraCarta {
     public static final String ANSI_YELLOW = "\u001B[33m";
     public static final String ANSI_BLUE = "\u001B[34m";
     public static final String ANSI_WHITE = "\u001B[37m";
-    public ImpresoraCarta(){};
+    public ImpresoraCarta() { };
+
+    private static final Map<Character, String> colorMap = new HashMap<Character, String>() {{
+        put('B', ANSI_BLUE);
+        put('R', ANSI_RED);
+        put('Y', ANSI_YELLOW);
+        put('G', ANSI_GREEN);
+    }};
 
 
     public void mostrarMazo(LinkedList<Carta> cartas){
-        String cartaActual;
-        char colorActual;
-        String codigoAnsi;
-        int indiceCartaActual = 0;
-        int sizeMazoCartas = cartas.size();
-        double ciclos = Math.ceil((double) sizeMazoCartas/3);
-        //Dividimos entre 3 y usamos 3 en los ciclos internos
-        //para mostrar las cartas de a tres, simple opcion estetica
-        for (int i = 0; i< ciclos;i++){
-
-            for(int j = 0; j <3; j++){
-                if(j==sizeMazoCartas){
-                    break;
-                }
+        for (int fila = 0; fila < Math.ceil(cartas.size() / 3.0); fila++){
+            for(int i = 0; i < Math.min(3, cartas.size() - fila * 3); i++)
                 System.out.print("+---+    ");
+            System.out.println();
+
+            for (Carta cartaActual : cartas.subList(fila * 3, Math.min(fila * 3 + 3, cartas.size()))) {
+                String cartaMostrar = String.format("%-3s", cartaActual.getColor() + cartaActual.getTipo());
+                String codigoAnsi = colorMap.getOrDefault(cartaActual.getColor(), ANSI_WHITE);
+                System.out.print("|" + codigoAnsi + cartaMostrar + ANSI_RESET + "|    ");
             }
             System.out.println();
 
-            for (int j = 0; j<3; j++){
-                if(j==sizeMazoCartas){
-                    break;
-                }
-
-                colorActual = cartas.get(indiceCartaActual).getColor();
-                switch (colorActual){
-                    case 'B':
-                        codigoAnsi = ANSI_BLUE;
-                        break;
-                    case 'R':
-                        codigoAnsi = ANSI_RED;
-                        break;
-                    case 'Y':
-                        codigoAnsi = ANSI_YELLOW;
-                        break;
-                    case 'G':
-                        codigoAnsi = ANSI_GREEN;
-                        break;
-                    default:
-                        codigoAnsi = ANSI_WHITE;
-                }
-
-                cartaActual = cartas.get(indiceCartaActual).getColor()+cartas.get(indiceCartaActual).getTipo();
-                if(cartaActual.length() <3){
-                    cartaActual = cartaActual + " ";
-                }
-                System.out.print("|"+codigoAnsi + cartaActual +ANSI_RESET+"|    ");
-                indiceCartaActual++;
-            }
-            System.out.println();
-
-            for(int j =0; j<3; j++){
-                if(j==sizeMazoCartas){
-                    return;
-                }
+            for(int i = 0; i < Math.min(3, cartas.size() - fila * 3); i++)
                 System.out.print("+---+    ");
-            }
             System.out.println();
-            sizeMazoCartas-=3;
         }
     }
 }
