7160014bab9644caf88f508ea627796f436e68c7
HumbertoAleman <haleman.23@est.ucab.edu.ve>
2024-06-02 11:24:58 -0400

    Quitado el metodo de getNumero y setNumero porque getNumero ya esta como getTipo, y setNumero no deberia poder ser usado

--- src/Juego/Carta/CartaNumerica.java
+++ src/Juego/Carta/CartaNumerica.java
@@ -2,32 +2,42 @@ package Juego.Carta;
 
 public class CartaNumerica extends CartaColorada {
     private String numero;
+
     /**
      * Constructor de la carta numerica
      */
     public CartaNumerica() {
     }
+
     /**
      * Constructor de la carta numerica
-     * @param color color de la carta
+     *
+     * @param color  color de la carta
      * @param numero numero de la carta
      */
     public CartaNumerica(char color, String numero) {
         super(color);
         this.numero = numero;
     }
+
     /**
      * Obtiene el numero de la carta
+     *
      * @return numero de la carta
      */
-    public String getNumero() { 
-        return this.numero; }
+    public String getNumero() {
+        return this.numero;
+    }
+
     /**
      * Asigna un numero a la carta
+     *
      * @param numero numero a asignar a la carta
      */
-    public void setNumero(String numero) { 
-        this.numero = numero; }
+    public void setNumero(String numero) {
+        this.numero = numero;
+    }
+
     /**
      * Ejecuta la accion de la carta
      */
@@ -35,19 +45,22 @@ public class CartaNumerica extends CartaColorada {
     public void ejecutarAccion() {
         // Las cartas numericas no tienen accion
     }
+
     /**
      * Obtiene el tipo de la carta
+     *
      * @return tipo de la carta
      */
     @Override
     public String getTipo() {
         return numero;
     }
+
     /**
      * Muestra la carta
      */
     @Override
-    public void mostrarCarta(){
+    public void mostrarCarta() {
         System.out.print(this.color + this.numero + "  ");
     }
 }
