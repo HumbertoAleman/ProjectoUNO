98ca83ca2788c4abb8ecd49924bd06b864ca42d2
HumbertoAleman <haleman.23@est.ucab.edu.ve>
2024-06-02 12:02:42 -0400

    Agregado metodo para que el computador muestre sus cartas

--- src/Juego/Jugador/Computador.java
+++ src/Juego/Jugador/Computador.java
@@ -1,25 +1,50 @@
 package Juego.Jugador;
 
 import Juego.Carta.Carta;
-import Juego.Carta.Comodin.CartaComodin;
-import Juego.Carta.Pila.PilaJugar;
 import Juego.Controlador.Juego;
 
 public class Computador extends Jugador {
+    private void mostrarTomarDecision() {
+        ImpresoraCarta.mostrarMazo(mazo, false);
+        System.out.print("El oponente esta tomando una seleccion");
+        try {
+            for (int i = 0; i < 3; i++) {
+                Thread.sleep(500);
+                System.out.print(".");
+            }
+            Thread.sleep(500);
+        } catch (Exception e) {
+            System.err.println(e);
+        }
+        System.out.println();
+    }
+
     @Override
     public void tomarTurno() {
+        mostrarTomarDecision();
         for (Carta carta : mazo) {
             if (Juego.jugarCarta(carta)) {
-                System.out.println("EL CPU JUGO");
                 mazo.remove(carta);
+                System.out.println("El oponente ha jugado la carta: " + carta.getColor() + carta.getTipo());
+                try {
+                    Thread.sleep(1500);
+                } catch (Exception e) {
+                    System.err.println(e);
+                }
                 return;
             }
+        }
+        System.out.println("El oponente no tiene cartas para jugar");
+        try {
+            Thread.sleep(1500);
+        } catch (Exception e) {
+            System.err.println(e);
         }
         Juego.darCartas(this);
     }
 
     @Override
-    public boolean cantarUno(){
+    public boolean cantarUno() {
         System.out.println("El COMPUTADOR CANTO UNO!");
         return true;
     }
