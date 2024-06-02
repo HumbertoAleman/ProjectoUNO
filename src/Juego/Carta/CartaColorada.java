c9ef7faa0ce99fad47b3e9b9d498db60cfa46288
HumbertoAleman <haleman.23@est.ucab.edu.ve>
2024-06-02 11:25:29 -0400

    Quitado el metodo setColor porque no se deberia poder cambiar el color de una carta instanciada

--- src/Juego/Carta/CartaColorada.java
+++ src/Juego/Carta/CartaColorada.java
@@ -2,28 +2,28 @@ package Juego.Carta;
 
 public abstract class CartaColorada implements Carta {
     protected char color;
+
     /**
      * Constructor de la carta colorada
      */
     public CartaColorada() {
     }
+
     /**
      * Constructor de la carta colorada
+     *
      * @param color color de la carta
      */
     public CartaColorada(char color) {
         this.color = color;
     }
+
     /**
      * Obtiene el color de la carta
+     *
      * @return color de la carta
      */
-    public char getColor() { 
-        return color; }
-    /**
-     * Asigna un color a la carta colorada
-     * @param color color a asignar a la carta
-     */
-    public void setColor(char color) { 
-        this.color = color; }
+    public char getColor() {
+        return color;
+    }
 }
