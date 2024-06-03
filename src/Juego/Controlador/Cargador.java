8fe42a0f4489bef902decd85bbb3603e4f4f7f03
Arthur-Pinto <10arturojpinto@gmail.com>
2024-06-03 11:51:23 -0400

    Avances en la clase cargador

--- src/Juego/Controlador/Cargador.java
+++ src/Juego/Controlador/Cargador.java
@@ -1,6 +1,15 @@
 package Juego.Controlador;
+import Juego.Carta.Accion.CartaMasDos;
+import Juego.Carta.Accion.CartaRevertir;
+import Juego.Carta.Accion.CartaSaltar;
+import Juego.Carta.Carta;
+import Juego.Carta.CartaNumerica;
+import Juego.Carta.Comodin.CartaCambiarColor;
+import Juego.Carta.Comodin.CartaMasCuatro;
 import Juego.Carta.Pila.PilaJugar;
 import Juego.Carta.Pila.PilaTomar;
+import Juego.Jugador.Humano;
+import Juego.Jugador.Jugador;
 import Juego.Jugador.Jugadores;
 import com.google.gson.Gson;
 
@@ -14,6 +23,9 @@ import java.io.IOException;
 
 public class Cargador {
     private Gson gson = new Gson();
+    private char color;
+    private String tipo;
+    private JSONObject carta;
     private String juego;
 
     public Cargador() {
@@ -21,19 +33,49 @@ public class Cargador {
     }
 
     public Jugadores cargarJugadores() throws IOException, ParseException {
+        Jugadores listaJugadores = new Jugadores();
         JSONParser jsonParser = new JSONParser();
         FileReader reader = new FileReader("C:\\Users\\10art\\Documents\\ProjectUNOCARDS\\src\\Juego\\Controlador\\listaJugadores.json");
         Object obj = jsonParser.parse(reader);
-        JSONObject auxiliar = (JSONObject) obj;
-        JSONArray lista = (JSONArray) auxiliar.get("listaJugadores");
-        for(int i =0;i<lista.size(); i++){
-            //Ya hay un JSONObject creado, necesitamos iterar sobre cada uno de los elementos del array
-            //Y agregarlos, no podemos mapear de una vez el array
-            //Intente usar la funcion de Gson que lo mapea si le pasas el nombre de la clase como argumento,
-            //pero con constructores vacios es asi imposible usar esa funcion
+        JSONObject objeto = (JSONObject) obj;
+        JSONArray listaJugadoresJson = (JSONArray) objeto.get("listaJugadores");
+        for(int i = 0; i<listaJugadoresJson.size(); i++){
+            JSONArray mazo = (JSONArray) listaJugadoresJson.get(i);
+            //Top idioteces que he hecho, para la proxima entrega deberiamos de colocar un distinctivo entre humanos y computadores en el JSON
+            //Para instanciarlos de manera distinta al crear. Si queda tiempo lo implementamos debidamente.
+            //Aqui se asume que el jugador en index 0 es el humano
+            if (i == 0){
+                Jugador humano = new Humano();
+                for (int j = 0; j<mazo.size(); j++){
+                    carta = (JSONObject) mazo.get(j);
+                    if(carta.containsKey("tipo") && carta.containsKey("colorSeleccionado") && carta.get("tipo") == "T4"){
+                        humano.agregarCarta(new CartaMasCuatro());
+                    } else if (carta.containsKey("tipo") && carta.containsKey("colorSeleccionado")) {
+                        humano.agregarCarta(new CartaCambiarColor());
+                    } else if(carta.containsKey("numero")){
+                        color = carta.get("color").toString().charAt(0);
+                        humano.agregarCarta(new CartaNumerica(color, carta.get("numero").toString()));
+                    }else if (carta.containsKey("tipo")){
+                        tipo = carta.get("tipo").toString();
+                        switch(tipo){
+                            case "R":
+                                humano.agregarCarta(new CartaRevertir(carta.get("color").toString().charAt(0)));
+                                break;
+                            case "T2":
+                                humano.agregarCarta(new CartaMasDos(carta.get("color").toString().charAt(0)));
+                                break;
+                            case "S":
+                                humano.agregarCarta(new CartaSaltar(carta.get("color").toString().charAt(0)));
+                        }
+
+                    }
+                }
+
+            }else{
+
+            }
+
         }
-        //Lo puse por ahora para poder correr el codigo de guardar
-        return null;
     }
 
 
