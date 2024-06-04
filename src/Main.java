3cbba9e0daf2eb8c719a8685877ee6f2048d7729
Arthur-Pinto <10arturojpinto@gmail.com>
2024-06-04 19:00:33 -0400

    Llamada a metodos de la interfaz en la clase Juego, uso de excepciones

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,12 @@
 import Juego.Controlador.Juego;
 
+import java.io.IOException;
+
 public class Main {
     /**
      * Main del programa
      */
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         boolean corriendo = true;
         while(corriendo) {
             corriendo = Juego.menuLoop();
